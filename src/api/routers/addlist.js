9f3c2beb16e9b06532edd597ed3bb9d3db78324c
qingShanBei <848771463@qq.com>
2019-04-11 22:15:58 +0800

    2.01

--- src/api/routers/addlist.js
+++ src/api/routers/addlist.js
@@ -19,7 +19,10 @@ router.post('/',async (ctx,next)=>{
 let {_id,goods_name,goods_price,goods_id,user_goods_limit_num,goods_image,goods_edittime,goods_salenum,gc_id_1_name}=ctx.request.body;
 // console.log('qqq',_id,title,pricing, sellingPrice,
 // repertory,img,uploadtime,salenum,classify)
-
+var date1 = goods_edittime;
+date1 = date1.substring(0,19);    
+date1 = date1.replace(/-/g,'/'); 
+goods_edittime = new Date(date1).getTime()/1000;
 let  res=await db.find('goodslist',{"_id":new ObjectId(_id)});
     // console.log(res.length,img);
 // 更新
